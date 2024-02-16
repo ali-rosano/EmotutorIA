e6e56eed927978caff7b6e13604151c20d373ddb
ali-rosano <109730338+ali-rosano@users.noreply.github.com>
2024-02-16 12:01:22 +0100

    Delete pages directory

--- pages/2_asignaturas.py
+++
@@ -1,9 +0,0 @@
-import streamlit as st
-
-def mostrar_asignaturas():
-    st.title("Sección de Asignaturas")
-    st.write("Aquí encontrarás ejercicios para diferentes asignaturas.")
-    # Agregar
-
-if __name__ == "__main__":
-    mostrar_asignaturas()
